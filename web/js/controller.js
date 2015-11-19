927df3b7eed444e7098fce94f620bd838268a5cf
Sadbot <semseriou@gmail.com>
2015-11-19 17:16:28 +0300

    19_11 userlist tasklist

--- web/js/controller.js
+++ web/js/controller.js
@@ -1,19 +1,37 @@
-var TaskTracker=angular.module('TaskTracker',[]);
+(function () {
 
-TaskTracker.controller('FirstCtrl', ['$scope', '$http', function ($scope, $http) {
+    var tt = angular.module('tt', []);
 
-        $scope.getTask = function () {
-            $http({
-                method: 'GET',
-                url: '/tasks'
-            })
-                    .then(function (response) {
-                        $scope.status = response.status;
-                        $scope.data = response.data;
-                    }, function (response) {
-                        $scope.data = response.data || "Request failed";
-                        $scope.status = response.status;
-                    });
-        };
+    tt.controller('TaskCtrl', ['$scope', '$http', function ($scope, $http) {
 
-    }]);
+            $scope.getTask = function () {
+                $http({
+                    method: 'GET',
+                    url: '/tasks'
+                })
+                        .then(function (response) {
+                            $scope.status = response.status;
+                            $scope.tasks = response.data;
+                        }, function (response) {
+                            $scope.tasks = response.data || "Request failed";
+                            $scope.status = response.status;
+                        });
+            };
+
+            $scope.getUser = function () {
+                $http({
+                    method: 'GET',
+                    url: '/users'
+                })
+                        .then(function (response) {
+                            $scope.status = response.status;
+                            $scope.users = response.data;
+                        }, function (response) {
+                            $scope.data = response.data || "Request failed";
+                            $scope.users = response.status;
+                        });
+            };
+
+        }]);
+
+})();
--- web/rest.php
+++ web/rest.php
@@ -8,7 +8,7 @@ define('DB_USER', 'root');
 define('DB_PASS', '071293');
 
 require_once __DIR__ . '/../vendor/autoload.php';
-require __DIR__ .'/TaskObject.php';
+require __DIR__ . '/Database.php';
 
 use Silex\Application;
 
@@ -18,17 +18,32 @@ $app['debug'] = true;
 
 $app->get('/tasks', function (Application $app) {
     
-    $taskModel = new TaskObject();
+    $db = new Database(DB_TYPE,DB_HOST,DB_NAME,DB_USER,DB_PASS);
     
-    $task = $taskModel->getAllFromTasks();
+    $task = $db->select('SELECT * from tasks');
 
     if (!$task) {
-        $error = array('message' => 'The task was not found.');
+        $error = array('message' => 'The tasks were not found.');
 
         return $app->json($error, 404);
     }
 
     return $app->json($task);
+});
+
+$app->get('/users', function (Application $app) {
+    
+    $db = new Database(DB_TYPE,DB_HOST,DB_NAME,DB_USER,DB_PASS);
+    
+    $user = $db->select('SELECT * from users');
+
+    if (!$user) {
+        $error = array('message' => 'The users were not found.');
+
+        return $app->json($error, 404);
+    }
+
+    return $app->json($user);
 });
 
 
